b8a503c75451fdfb585cc9f4250cebeb6cb45887
alexluong <aluong@crimson.ua.edu>
2018-07-24 15:24:08 -0500

    Set up demo website + Found out inconsistence in API, probably need to revisit it

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Source Dog',
+    title: "Gatsby Source Dog",
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-emotion",
+    {
+      resolve: "gatsby-source-dog",
+      options: {
+        breeds: {
+          random: true,
+          number: 5,
+        },
+      },
+    },
+  ],
 }
--- www/src/components/Layout.js
+++ www/src/components/Layout.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { StaticQuery, graphql } from "gatsby"
 
-import Header from './Header'
-import './Layout.css'
+import Header from "./Header"
+import resetCss from "config/resetCss"
+
+resetCss()
 
 const Layout = ({ children, data }) => (
   <StaticQuery
@@ -22,16 +24,16 @@ const Layout = ({ children, data }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: "description", content: "Sample" },
+            { name: "keywords", content: "sample, something" },
           ]}
         />
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: '0 auto',
+            margin: "0 auto",
             maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
+            padding: "0px 1.0875rem 1.45rem",
             paddingTop: 0,
           }}
         >
