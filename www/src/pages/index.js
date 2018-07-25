6068d9ea9d1bef1c26621ce74e9d21447dd66576
alexluong <aluong@crimson.ua.edu>
2018-07-25 16:21:12 -0500

    Keep working on demo + There's a TODO

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
+import Link from "gatsby-link"
 import Layout from "components/Layout"
 
 const IndexPage = ({ imgURL }) => (
@@ -11,17 +12,41 @@ const IndexPage = ({ imgURL }) => (
 
     <img src={imgURL} alt="Some random beautiful dog" />
 
-    <h2>To install the plugin, run:</h2>
+    <h2>Documentation</h2>
+    <p>
+      For a detailed explanation of the API, please read through the{" "}
+      <Link to="/doc">documentation page</Link>
+    </p>
+
+    <h2>Quick Start</h2>
+
+    <h3>Installation</h3>
     <pre>yarn add gatsby-source-dog</pre>
 
-    <h2>Set up</h2>
+    <h3>Config</h3>
     <p>
       Inside your <code>gatsby-config.js</code>:
     </p>
     <pre>{config}</pre>
+
+    <h3>Query</h3>
+    <p>In this page, I randomly display a dog image. Here is how I did it:</p>
+    <ol>
+      <li>Get all dog images</li>
+      <li>Randomly select one image</li>
+    </ol>
+    <pre>{randomImage}</pre>
+
+    <p>For more examples, please check out:</p>
+    <ul>
+      <li>Example 1</li>
+      <li>Example 2</li>
+      <li>Example 3</li>
+    </ul>
   </Layout>
 )
 
+// TODO: Make this a HOC
 export default props => (
   <StaticQuery
     query={graphql`
@@ -46,6 +71,7 @@ export default props => (
 
 const config = `
 // gatsby-config.js:
+
 {
   ...
   plugins: [
@@ -80,4 +106,28 @@ const config = `
   ],
   ...
 }
+`
+
+const randomImage = `
+// src/pages/index.js
+
+<StaticQuery
+  query={graphql\`
+    {
+      allDogImage {
+        edges {
+          node {
+            url
+          }
+        }
+      }
+    }
+  \`}
+  render={data => {
+    const imgArr = data.allDogImage.edges
+    const randomURL =
+      imgArr[Math.floor(Math.random() * imgArr.length)].node.url
+    return <IndexPage imgURL={randomURL} {...props} />
+  }}
+/>
 `
